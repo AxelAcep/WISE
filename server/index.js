ea85dc1873169bbace9c5d9094b1939e98c76f81
Seruzu <138269063+AxelAcep@users.noreply.github.com>
2024-06-19 17:39:58 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,42 +1,49 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const app = express();
 const dotenv = require('dotenv').config(); 
-const clientPath = path.join(__dirname, '/public');
+
+const app = express();
 
 const homeRoute = require('./routes/homepage');
 const userRoute = require('./routes/userpage');
 const userprofileRoute = require('./routes/userprofilepage');
 const useracaraRoute = require('./routes/useracara');
-
 const mitraRoute = require('./routes/mitrapage');
 const mitraprofileRoute = require('./routes/mitraprofilepage');
-const addacararoute = require('./routes/addacarapage')
-const editacararoute = require('./routes/editacara')
+const addacararoute = require('./routes/addacarapage');
+const editacararoute = require('./routes/editacara');
 const editpending = require('./routes/editpending');
 const userhistoryRoute = require('./routes/riwayapage');
-
 const adminacaraRoute = require('./routes/adminpage');
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + "/public/"))
 
+// Middleware untuk melayani berkas statis dari direktori 'public'
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/asset', express.static(path.join(__dirname, 'public/asset')));
+app.use('/Data', express.static(path.join(__dirname, 'public/Data')));
+
+// Routes
 app.use('/', homeRoute);
-app.use('/user', userRoute)
+app.use('/user', userRoute);
 app.use('/userprofile', userprofileRoute);
-app.use('/useracara', useracaraRoute)
-app.use('/riwayat', userhistoryRoute)
-
+app.use('/useracara', useracaraRoute);
+app.use('/riwayat', userhistoryRoute);
 app.use('/mitra', mitraRoute);
 app.use('/mitraprofile', mitraprofileRoute);
 app.use('/addacarapage', addacararoute);
 app.use('/editacarapage', editacararoute);
 app.use('/editpending', editpending);
+app.use('/admin', adminacaraRoute);
 
-app.use('/admin', adminacaraRoute)
+// Start server
+const port = process.env.PORT || 4001;
+app.listen(port, () => {
+  console.log("Server berjalan pada port:", port);
+});
 
-app.listen(4001, async () => {
-    console.log("Server berjalan pada port:", process.env.PORT );
-});    
+module.exports = app;
